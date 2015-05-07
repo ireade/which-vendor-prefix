7f24cc113fa2af5bd5fd3577c82844bc8c6e9724
Ire Aderinokun <ire@ireaderinokun.com>
2015-05-07 08:58:54 +0100

    removed non-css related properties

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Displays all the css properties that need a vendor prefix. Based on [caniuse](ht
 
 Still a work in progress.
 
-I created this by looking through the caniuse data for anything that shows to require a vendor prefix. For some reason, things other than css properties show up on the list as well.
 
-Also trying to work out how to use the caniuse data directly. At the moment I have had to download a local version because of the following error:
+
+Trying to work out how to use the caniuse data directly. At the moment I have had to download a local version because of the following error:
 
 "XMLHttpRequest cannot load https://raw.githubusercontent.com/Fyrd/caniuse/master/data.json. The 'Access-Control-Allow-Origin' header has a value 'https://render.githubusercontent.com' that is not equal to the supplied origin. Origin 'null' is therefore not allowed access."
 
--- js/script-alt.js
+++ js/script-alt.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+	// Alternate Method - Gets all CSS related categories, regardles of whether any browser version needs a vendor prefix
 
 	var requestUrl = 'http://raw.githubusercontent.com/Fyrd/caniuse/master/data.json';
 	var localRequestUrl = 'data.json';
@@ -19,33 +20,13 @@ $(document).ready(function() {
 			// If has CSS related category
 			if ( !($.inArray('CSS', value.categories) == -1) | !($.inArray('CSS2', value.categories) == -1) | !($.inArray('CSS3', value.categories) == -1) ) {
 			
-				// Loop through each browser
-				$.each(value.stats, function(i, v) {
-
-					//Loop through each version
-					$.each(v, function(version, support) {
-
-						if ( support == 'y x' ) {
-							cssProperties.push(value);
-							added = true;
-						}
-
-						// If property is already added to list, break out of version loop
-						if (added) { return false; }
-					})
-
-					// If property is already added to list, break out of browser loop
-					if (added) { return false; }
-
-				}); 
-
+				cssProperties.push(value);
+				added = true;
 			}
 
-	
 		})
 
 
-		
 		var source = $("#properties-template").html()
 		var template = Handlebars.compile(source)
 		var output = template( {Property: cssProperties} )
